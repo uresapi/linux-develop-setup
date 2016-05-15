fd70329c686740e7e1d30eb2a1bece0696b1fda7
Georgi Sabev <georgethebeatle@gmail.com>
2016-05-15 13:17:00 +0300

    Add tools for modifying key behaviour

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ set -e -x
 
 sudo apt-get update
 
+# Make Caps Lock a second Ctrl (http://askubuntu.com/questions/462021/how-do-i-turn-caps-lock-into-an-extra-control-key)
+sudo apt-get install gnome-tweak-tool
+
+# Prevent Alt from displaying the HUD (http://askubuntu.com/questions/151951/how-do-i-prevent-ubuntu-from-capturing-the-alt-key-to-show-the-hud)
+sudo apt-get -y install compizconfig-settings-manager
+
 # curl
 sudo apt-get -y install curl
 
