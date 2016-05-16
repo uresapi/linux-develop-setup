e52f6ba64e7dd5a5f3ad6ebd595bfe7af2269b4a
Georgi Sabev <georgethebeatle@gmail.com>
2016-05-16 08:46:14 +0300

    Install bash-it

--- install.sh
+++ install.sh
@@ -10,6 +10,14 @@ sudo apt-get install gnome-tweak-tool
 # Prevent Alt from displaying the HUD (http://askubuntu.com/questions/151951/how-do-i-prevent-ubuntu-from-capturing-the-alt-key-to-show-the-hud)
 sudo apt-get -y install compizconfig-settings-manager
 
+# bash-it
+if [ ! -d ~/.bash_it ]; then
+  git clone --depth=1 https://github.com/Bash-it/bash-it.git $HOME/.bash_it
+  $HOME/.bash_it/install.sh --none
+fi
+
+cp "$PWD"/bash-it-custom/*.bash "$HOME"/.bash_it/custom/
+
 # curl
 sudo apt-get -y install curl
 
