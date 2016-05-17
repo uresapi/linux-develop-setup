a70adb266b755fb349a307676b31b98a58e2bab3
Georgi Sabev <georgethebeatle@gmail.com>
2016-05-17 18:24:04 +0300

    Install vlc player

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e -x
 sudo apt-get update
 
 # Make Caps Lock a second Ctrl (http://askubuntu.com/questions/462021/how-do-i-turn-caps-lock-into-an-extra-control-key)
-sudo apt-get install gnome-tweak-tool
+sudo apt-get -y install gnome-tweak-tool
 
 # Prevent Alt from displaying the HUD (http://askubuntu.com/questions/151951/how-do-i-prevent-ubuntu-from-capturing-the-alt-key-to-show-the-hud)
 sudo apt-get -y install compizconfig-settings-manager
@@ -40,3 +40,5 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo add-apt-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
 sudo apt-get update && sudo apt-get -y install google-chrome-stable
 
+# vlc
+sudo apt-get -y install vlc
