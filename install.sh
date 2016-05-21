04693e203e60a76f6da2be8ef7d0e8f49d3e1287
Zhou Yu <jutkko@gmail.com>
2016-05-21 17:24:10 +0100

    Add scripts to setup the machine remotely

--- install.sh
+++ install.sh
@@ -4,14 +4,21 @@ set -e -x
 
 sudo apt-get update
 
+# install the locale to get rid of the warning
+sudo apt-get -y install language-pack-en
+
 # curl
 sudo apt-get -y install curl
 
+# git
+sudo apt-get -y install git
+
 # vim
 sudo apt-get -y install vim
-stat ~/.vim || curl vimfiles.luan.sh/install | bash
-~/.vim/update
 
 # tmux
 sudo apt-get -y install tmux
 wget -O ~/.tmux.conf https://raw.githubusercontent.com/luan/dotfiles/master/tmux.conf
+
+# golang
+sudo apt-get -y install golang-1.6
