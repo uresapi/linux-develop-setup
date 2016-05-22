f5b57888f06f5e5bc372c40d19125ac307a85732
Zhou Yu <jutkko@gmail.com>
2016-05-22 13:56:13 +0100

    Add golang support in the script

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #/!/bin/bash
+GOFILE=go1.6.2.linux-amd64.tar.gz
 
 set -e -x
 
 sudo apt-get update
+sudo apt-get -y upgrade
 
 # install the locale to get rid of the warning
 sudo apt-get -y install language-pack-en
@@ -21,4 +23,22 @@ sudo apt-get -y install tmux
 wget -O ~/.tmux.conf https://raw.githubusercontent.com/luan/dotfiles/master/tmux.conf
 
 # golang
-sudo apt-get -y install golang-1.6
+sudo curl -O https://storage.googleapis.com/golang/$GOFILE
+sudo tar -xvf $GOFILE
+# clean the previous versions
+sudo rm -rf /usr/local/go
+sudo mv go /usr/local
+sudo rm -rf $GOFILE
+
+# setup go
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+go get github.com/tools/godep
+
+mkdir -p $HOME/go/src/github.com/uresapi
+pushd $HOME/go/src/github.com/uresapi
+git clone https://github.com/uresapi/fbm-bot.git
+cd fbm-bot
+godep restore
+popd
